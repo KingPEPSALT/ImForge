6ae7d8afbc8df1498b0436c0643fd195966d4e85
KingPEPSALT <68469008+KingPEPSALT@users.noreply.github.com>
2023-06-26 17:57:12 +0100

    refactor(callback): refactored NCallback
    refactored using the proposed change in #2

--- include/core/callback.hpp
+++ include/core/callback.hpp
@@ -20,22 +20,25 @@ protected:
     std::function<void(Args...)> function;
 };
 
-template <size_t N, typename... Args>
+template <typename... Args>
 struct NCallback : Callback<Args...> {
-    NCallback(std::function<void(Args...)> function)
-        : Callback<Args...>(function), times(0) {};
+
+    NCallback(std::function<void(Args...)> function, size_t n)
+        : Callback<Args...>(function), n(n) {};
     
     inline void operator ()(Args... args){
-        if(this->times++ > N) return;
+        if(!this->n--) return;
         this->function(args...);
     }
+
 private:
-    size_t times; 
+    size_t n; 
 };
 
 template <typename... Args>
-struct NCallback<1, Args...> : Callback<Args...> {
-    NCallback(std::function<void(Args...)> function)
+struct OnceCallback : Callback<Args...> {
+
+    OnceCallback(std::function<void(Args...)> function)
         : Callback<Args...>(function), used(false) {};
     
     inline void operator ()(Args... args){
@@ -43,15 +46,14 @@ struct NCallback<1, Args...> : Callback<Args...> {
         this->function(args...);
         this->used = true;
     }
+
 private:
     bool used;
 };
 
 template <typename... Args>
-using OnceCallback = NCallback<1, Args...>;
-
-template <typename... Args>
 struct OnFirstCallback : Callback<Args...> {
+
     OnFirstCallback(
         std::function<void(Args...)> function,
         std::function<void(Args...)> on_first
@@ -64,6 +66,7 @@ struct OnFirstCallback : Callback<Args...> {
         }
         return this->function();
     }
+
 private:
     std::function<void(Args...)> on_first;
     bool used; 
@@ -71,6 +74,7 @@ private:
 
 template <typename... Args>
 struct Callbacks {
+    
     inline void invoke(std::string id, Args... args) {
         if(!this->callbacks.contains(id) || !this->callbacks.at(id))
             return;
